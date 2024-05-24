132086754b7b02c01ea5cadfc54d321356beda84
Mahabad <58578130+mbarwari@users.noreply.github.com>
2024-05-23 22:38:17 -0500

    Update ABP_sensor.ino
    added averaging of the sensor value readings

--- ABP_sensor/ABP_sensor.ino
+++ ABP_sensor/ABP_sensor.ino
@@ -13,8 +13,9 @@ Sensor       Arduino
 5.NC         ->   NC
 6.Vsupply    ->   5V
 
-Add 0.001 uf capacitor between Vout and GND
-Add 0.1 uf capacitor between Vsupply and GND 
+Reduce noise as per the datasheet: 
+  Add 0.001 uf capacitor between Vout and GND
+  Add 0.1 uf capacitor between Vsupply and GND 
 
 */
 
@@ -22,40 +23,47 @@ Add 0.1 uf capacitor between Vsupply and GND
 const int pressureSensorPin = A0;
 
 // Sensor specifications
-const float Vsupply = 5.0; // Supply voltage
-const float Pmin = 0; // Minimum pressure in PSI
-const float Pmax = 15; // Maximum pressure in PSI
+const float Vsupply = 5.0;   // Supply voltage
+const float Pmin = 0;        // Minimum pressure in PSI
+const float Pmax = 15;       // Maximum pressure in PSI
+
+const int sampleSize = 10;   // Sample size  
+const int decimalPlaces = 3; // Decimal places for Serial.print() 
 
 
 void setup() {
-  analogReadResolution(14); // Increase dafault 10-bit ADC resolution to 14-bit 
-  Serial.begin(9600); // Initialize serial communication
-
+  analogReadResolution(14);  // Increase dafault 10-bit ADC resolution to 14-bit
+  Serial.begin(9600);        // Initialize serial communication
 }
 
 void loop() {
-  // Read the voltage from the pressure sensor
-  // analogRead() returns an integer between 0-16383
-  int sensorValue = analogRead(pressureSensorPin); 
+
+  int total = 0;
+
+  for (int i = 0; i < sampleSize; i++) {
+    // Read the voltage from the pressure sensor
+    // analogRead() returns an integer between 0-16383
+    int sensorValue = analogRead(pressureSensorPin);
+    total += sensorValue;
+    delay(100);
+  }
+
+  // Find the average of the sensor value readings 
+  int averageSensorValue = total / sampleSize;
 
   // Convert the analog reading to voltage (0-5V)
-  float outputVoltage = sensorValue * (5.0  / 16383.0);
+  float outputVoltage = averageSensorValue * (5.0 / 16383.0);
 
   // Print the voltage to the serial monitor
-  Serial.print(outputVoltage);
+  Serial.print(outputVoltage, decimalPlaces);
 
-  // pressureApplied = 15/(0.8*5)*(Vout-0.5) + 0 
-  float pressureApplied = 15/(0.8*5)*(outputVoltage-0.5) + 0;
+  // pressureApplied = 15/(0.8*5)*(Vout-0.5) + 0
+  float pressureApplied = 15 / (0.8 * 5) * (outputVoltage - 0.5) + 0;
 
   // Print the pressure to the serial monitor
   Serial.print(" ");
-  Serial.println(pressureApplied);
+  Serial.println(pressureApplied, decimalPlaces);
 
-  // Delay for a short period before the next reading
-  delay(1000);
-
+  // Delay for 100 miliseconds before the next set of readings 
+  delay(100);
 }
-
-
-
-
