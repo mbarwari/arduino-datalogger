67a993990bfc5cb2db7de9b8db759bf0eea9b3a4
Mahabad <58578130+mbarwari@users.noreply.github.com>
2024-05-23 20:40:59 -0500

    Update ABP_sensor.ino
    update voltage to pressure equation

--- ABP_sensor/ABP_sensor.ino
+++ ABP_sensor/ABP_sensor.ino
@@ -26,10 +26,6 @@ const float Vsupply = 5.0; // Supply voltage
 const float Pmin = 0; // Minimum pressure in PSI
 const float Pmax = 15; // Maximum pressure in PSI
 
-/*
-const float Vmin = 0.10 * Vsupply; // Minimum output voltage
-const float Vmax = 0.90 * Vsupply; // Maximum output voltage
-*/
 
 void setup() {
   analogReadResolution(14); // Increase dafault 10-bit ADC resolution to 14-bit 
@@ -41,31 +37,19 @@ void loop() {
   // Read the voltage from the pressure sensor
   // analogRead() returns an integer between 0-16383
   int sensorValue = analogRead(pressureSensorPin); 
-  
-  // Print the analogRead value to the serial monitor
-  Serial.print("analogRead value: ");
-  Serial.println(sensorValue);
 
   // Convert the analog reading to voltage (0-5V)
   float outputVoltage = sensorValue * (5.0  / 16383.0);
 
   // Print the voltage to the serial monitor
-  Serial.print("Output Voltage: ");
   Serial.print(outputVoltage);
-  Serial.println(" V");
-  
-  /*
-  // pressureApplied = ((Pmax - Pmin) / (Vmax - Vmin)) * (outputVoltage - Vmin) + Pmin 
-  float pressureApplied = ((Pmax - Pmin) / (Vmax - Vmin)) * (outputVoltage - Vmin) + Pmin;
-  */ 
 
-  // pressureApplied = 15/(0.8*5)*(Vout-0.05) + 0 
-  float pressureApplied = 15/(0.8*5)*(outputVoltage-0.05) + 0;
+  // pressureApplied = 15/(0.8*5)*(Vout-0.5) + 0 
+  float pressureApplied = 15/(0.8*5)*(outputVoltage-0.5) + 0;
 
   // Print the pressure to the serial monitor
-  Serial.print("Pressure Applied: ");
-  Serial.print(pressureApplied);
-  Serial.println(" PSI");
+  Serial.print(" ");
+  Serial.println(pressureApplied);
 
   // Delay for a short period before the next reading
   delay(1000);
