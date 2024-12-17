21dfb422dc50991a6a172950230bdbb2b672d7e5
Mahabad <58578130+mbarwari@users.noreply.github.com>
2024-12-17 12:31:42 -0600

    Update SD_mod_esp32.ino

--- Sensors/SD_mod_esp32/SD_mod_esp32.ino
+++ Sensors/SD_mod_esp32/SD_mod_esp32.ino
@@ -51,6 +51,71 @@ int mosi = -1;
 int cs = -1;
 */
 
+
+void setup() {
+  
+  Serial.begin(9600);
+  while (!Serial) {
+    delay(10);
+  }
+
+  #ifdef REASSIGN_PINS
+    SPI.begin(sck, miso, mosi, cs);
+    if (!SD.begin(cs)) {
+  #else
+    if (!SD.begin()) {
+  #endif
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+
+  if (cardType == CARD_NONE) {
+    Serial.println("No SD card attached");
+    return;
+  }
+
+  Serial.print("SD Card Type: ");
+  if (cardType == CARD_MMC) {
+    Serial.println("MMC");
+  } else if (cardType == CARD_SD) {
+    Serial.println("SDSC");
+  } else if (cardType == CARD_SDHC) {
+    Serial.println("SDHC");
+  } else {
+    Serial.println("UNKNOWN");
+  }
+
+  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
+  Serial.printf("SD Card Size: %lluMB\n", cardSize);
+
+  listDir(SD, "/", 0);
+  writeFile(SD, "/data.txt", "");
+  appendFile(SD, "/data.txt", "DATA FILE\n");
+  readFile(SD, "/data.txt");
+  testFileIO(SD, "/data.txt");
+  Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
+  Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
+}
+
+
+void loop() {
+
+  String data = ""; 
+
+  for(int i=0; i<10; i++){
+    data += String(i);
+    data += "\n";
+    appendFile(SD, "/data.txt", data.c_str());
+    data = ""; 
+  }
+
+  while(1){
+  }
+
+}
+
+
 void listDir(fs::FS &fs, const char *dirname, uint8_t levels) {
   Serial.printf("Listing directory: %s\n", dirname);
 
@@ -207,56 +272,3 @@ void testFileIO(fs::FS &fs, const char *path) {
   file.close();
 }
 
-void setup() {
-  Serial.begin(115200);
-  while (!Serial) {
-    delay(10);
-  }
-
-#ifdef REASSIGN_PINS
-  SPI.begin(sck, miso, mosi, cs);
-  if (!SD.begin(cs)) {
-#else
-  if (!SD.begin()) {
-#endif
-    Serial.println("Card Mount Failed");
-    return;
-  }
-  uint8_t cardType = SD.cardType();
-
-  if (cardType == CARD_NONE) {
-    Serial.println("No SD card attached");
-    return;
-  }
-
-  Serial.print("SD Card Type: ");
-  if (cardType == CARD_MMC) {
-    Serial.println("MMC");
-  } else if (cardType == CARD_SD) {
-    Serial.println("SDSC");
-  } else if (cardType == CARD_SDHC) {
-    Serial.println("SDHC");
-  } else {
-    Serial.println("UNKNOWN");
-  }
-
-  uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-  Serial.printf("SD Card Size: %lluMB\n", cardSize);
-
-  listDir(SD, "/", 0);
-  createDir(SD, "/mydir");
-  listDir(SD, "/", 0);
-  removeDir(SD, "/mydir");
-  listDir(SD, "/", 2);
-  writeFile(SD, "/hello.txt", "Hello ");
-  appendFile(SD, "/hello.txt", "World!\n");
-  readFile(SD, "/hello.txt");
-  deleteFile(SD, "/foo.txt");
-  renameFile(SD, "/hello.txt", "/foo.txt");
-  readFile(SD, "/foo.txt");
-  testFileIO(SD, "/test.txt");
-  Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
-  Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
-}
-
-void loop() {}
